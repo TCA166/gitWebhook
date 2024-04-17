cb5ca488994a6608aebb28c3eef52b0af5ff33d1
TCA166 <tomek.chady@gmail.com>
2024-04-17 15:19:48 +0200

    Customization improvements, added optional logging

--- webhook.py
+++ webhook.py
@@ -3,10 +3,16 @@ from hashlib import sha256
 from hmac import new as hmacNew
 from subprocess import run
 from unittest import TestSuite, TestResult
+from typing import Any
+from logging import Logger
+
+GITHUB_HEADER = "X-Hub-Signature-256"
+
+GITLAB_HEADER = "X-Gitlab-Token"
 
 def verifyGithubSignature(request: Request, token:str) -> bool:
     """Verify the GitHub signature of a webhook request"""
-    signature = request.headers.get("X-Hub-Signature-256")
+    signature = request.headers.get(GITHUB_HEADER)
     if signature is None:
         return False
     hash_object = hmacNew(token.encode("utf-8"), msg=request.get_data(), digestmod=sha256)
@@ -15,34 +21,72 @@ def verifyGithubSignature(request: Request, token:str) -> bool:
 
 class webhookBlueprint(Blueprint):
     """Wrapper over the flask blueprint that creates an endpoint for receiving and processing git webhooks. Overwrite the processWebhook method to process the webhook data."""
-    def __init__(self, webhookToken:str, tests:TestSuite=None, name:str="webhook", import_name:str=__name__, *args, **kwargs):
+    def __init__(self, webhookToken:str, tests:TestSuite | None = None, log:Logger | None = None, name:str="webhook", import_name:str=__name__, gitCommand:str="/usr/bin/git", commandEnv:dict | None = None, *args, **kwargs):
         super().__init__(name, import_name, *args, **kwargs)
         self.webhookToken = webhookToken
         self.tests = tests
+        self.gitCommand = gitCommand
+        self.log = log
+        if commandEnv is None:
+            commandEnv = dict(GIT_SSH_COMMAND="/usr/bin/ssh")
+        self.commandEnv = commandEnv
         self.route("/", methods=["POST"])(self.receiveWebhook)
     def receiveWebhook(self) -> Response:
         """Receive webhook from GitHub and process it using the processWebhook method."""
-        if "X-Hub-Signature-256" in request.headers:
+        if self.log is not None:
+            self.log.debug("Received a POST request to the webhook endpoint")
+        #check if the content type is json
+        if request.content_type != "application/json":
+            if self.log is not None:
+                self.log.warning(f"A request with an invalid content type: {request.content_type}")
+            abort(415)
+        if GITHUB_HEADER in request.headers:
             if not verifyGithubSignature(request, self.webhookToken):
+                if self.log is not None:
+                    self.log.warning("A request with an invalid GitHub signature")
                 abort(401)
-        elif "X-Gitlab-Token":
-            if request.headers.get("X-Gitlab-Token") != self.token:
+        elif GITLAB_HEADER:
+            if request.headers.get(GITLAB_HEADER) != self.webhookToken:
+                if self.log is not None:
+                    self.log.warning("A request with an invalid GitLab token")
                 abort(401)
         else:
-            abort(400, "Unsupported webhook source")
+            if self.log is not None:
+                self.log.warning("A request with no signature found")
+            abort(401) #no feedback, in case somebody is trying to guess the token
+        #logs beforehand were warnings, so that messages regarding unauthorized requests can be filtered
+        data = request.json
+        if data is None or not isinstance(data, dict):
+            if self.log is not None:
+                self.log.error("A request with invalid JSON")
+            abort(400, "Invalid JSON")
         #at this point the webhook is verified
-        return self.processWebhook(request.json)
-    def processWebhook(self, data:dict) -> tuple[int, str]:
+        ret = self.processWebhook(data)
+        if self.log is not None:
+            self.log.info(f"Webhook processed with status code {ret[0]} and message: {ret[1]}")
+        return Response(ret[1], status=ret[0])
+    def processWebhook(self, data:dict[str, Any]) -> tuple[int, str]:
         """Process the webhook. Return a tuple of (status code, message)"""
-        process = run(["/usr/bin/git", "pull"], env=dict(GIT_SSH_COMMAND="/usr/bin/ssh"))
+        if self.log is not None:
+            self.log.debug(f"Processing webhook: {data}")
+        process = run([self.gitCommand, "pull"], env=self.commandEnv)
         if process.returncode != 0:
+            if self.log is not None:
+                self.log.error(f"Error while pulling: {process.stderr.decode('utf-8')}")
             return 500, process.stderr.decode("utf-8")
         if self.tests is not None:
-            result:TestResult = self.tests.run()
+            if self.log is not None:
+                self.log.debug("Running tests")
+            result = TestResult()
+            self.tests.run(result) # Why does this method want a TestResult object? Why can't it just return the result?
             if result.wasSuccessful():
+                if self.log is not None:
+                    self.log.info(f"{result.testsRun} tests passed ")
                 return 200, "Tests passed"
             else:
-                abortProcess = run(["/usr/bin/git", "merge", "--abort"], env=dict(GIT_SSH_COMMAND="/usr/bin/ssh"))
+                abortProcess = run([self.gitCommand, "merge", "--abort"], env=self.commandEnv)
+                if self.log is not None:
+                    self.log.error(f"Tests did not pass, Errors: {result.errors}, Failures: {result.failures}. Merge abort status: {abortProcess.returncode}")
                 return 428, f"Tests did not pass, Errors: {result.errors}, Failures: {result.failures}. Merge abort status: {abortProcess.returncode}"
         else:
             return 200, "Webhook received successfully"
